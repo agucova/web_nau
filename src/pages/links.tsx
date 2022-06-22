530d81526334e2c86071d5a36a86feaa28634e6f
Agustín Covarrubias <agucova@uc.cl>
2022-06-22 19:16:22 -0400

    init links

---
+++ src/pages/links.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import { Container, Heading, Text } from '@chakra-ui/react'
+
+const LinksPage = () => {
+    return (
+        <Layout pageTitle="Links 🔗">
+            <Container minH="3xl">
+                <Heading>Links 🔗</Heading>
+                <Text>A</Text>
+            </Container>
+        </Layout>
+    )
+}
+
+export default LinksPage
